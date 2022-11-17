7015858809871f871a16380f2ecd23e08b4fee9e
Facug03 <facundogonzales1550@gmail.com>
2022-11-17 20:51:39 -0300

    problem of scrolling resolved

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -3,14 +3,17 @@ import { Route, Routes } from 'react-router-dom'
 import Nav from './components/Nav'
 import Games from './components/Games'
 import Detail from './components/Detail'
+import ScrollToTop from './helpers/ScrollToTop'
 
 export default function App() {
   return (
-    <div className='bg-backcolor'>
+    <div className='bg-backcolor px-14'>
+      <ScrollToTop />
       <Nav />
       <Routes>
         <Route path='/' element={<Games />} />
-        <Route path='/detail' element={<Detail />} />
+        <Route path='/game/:id' element={<Detail />} />
+        {/* <Route path="*" component={NotFound} /> */}
       </Routes>
     </div>
   )
